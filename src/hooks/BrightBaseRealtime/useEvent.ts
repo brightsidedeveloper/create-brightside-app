28e30a067963a19a568da522de9ad21fcc103e08
tim <tim@brightsidedeveloper.com>
2024-08-18 15:50:27 -0500

    Fixed Type Error

--- src/hooks/BrightBaseRealtime/useEvent.ts
+++ src/hooks/BrightBaseRealtime/useEvent.ts
@@ -1,6 +1,6 @@
+import { EventCallback } from '@/types/bright.types'
 import { BrightBaseRealtime } from 'brightside-developer'
 import { useEffect, useRef } from 'react'
-import { EventCallback } from '../../api/Channels'
 
 export default function useEvent<T extends { [event: string]: unknown }, K extends keyof T>(
   channel: BrightBaseRealtime<T>,
